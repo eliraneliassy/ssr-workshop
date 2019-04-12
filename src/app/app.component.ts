fa7568c14fcabf067f43455dd5c4e77081c2bd1b
Eliran <eliran.eliassy-external@simplex.com>
2019-04-12 19:02:30 +0300

    step 1 - app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { FeedService } from './feed.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ssr-workshop';
+export class AppComponent implements OnInit {
+  items$: Observable<Item[]>;
+  constructor(private feedService: FeedService) { }
+  ngOnInit(): void {
+    this.items$ = this.feedService.getFeed(0);
+  }
 }
---
+++ src/app/feed.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Item } from './item';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getFeed(page: number = 0): Observable<Item[]> {
+    return this.httpClient.get<Item[]>(`https://api.fashbash.co/api/feed?page=${page}`);
+  }
+}
