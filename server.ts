59ee0229a79d58038f4a1ab42dd05b871ddb35b2
= <eliran.eliassy@gmail.com>
2019-04-13 22:32:33 +0300

    nguniversal

--- server.ts
+++ server.ts
@@ -1,40 +1,44 @@
+
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+
 import { renderModuleFactory } from '@angular/platform-server';
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
-const { AppServerModuleNgFactory } = require('./dist/ssr-workshop-server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/ssr-workshop-server/main');
 
 import * as express from 'express';
 import { readFileSync } from 'fs';
 import { enableProdMode } from './node_modules/@angular/core';
+import { AotCompiler } from '@angular/compiler';
 
 enableProdMode();
 
 const app = express();
-const indexHtml = readFileSync(__dirname + '/dist/ssr-workshop/index.html', 'utf-8').toString();
 
+const distFoler = __dirname + '/dist/ssr-workshop';
 
-app.get('*.*', express.static(__dirname + '/dist/ssr-workshop', {
-   maxAge: '5y'
+app.engine('html', ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)]
 }));
 
-app.route('*').get((req, res) => {
+app.set('view engine', 'html');
 
-   renderModuleFactory(AppServerModuleNgFactory, {
-       document: indexHtml,
-       url: req.url
-   })
-       .then(html => res.status(200).send(html))
-       .catch(err => {
-           console.log(err);
-           res.sendStatus(500);
-       });
+app.set('views', __dirname + '/dist/ssr-workshop');
 
 
+app.get('*.*', express.static(distFoler, {
+    maxAge: '1y'
+}));
+
+
+app.get('*', (req, res) => {
+    res.render('index', { req });
 });
 
-
 app.listen(4200, () => {
-   console.log('express running on port 4200');
+    console.log('express running on port 4200');
 });
 
